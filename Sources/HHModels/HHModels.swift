f16967b0a055ed3b7a25d3ba411b4d64e1ff5f07
mchm <m@mchm.pl>
2025-12-29 16:29:14 +0100

    Bug fix

--- Sources/HHModels/HHModels.swift
+++ Sources/HHModels/HHModels.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+struct Product: Identifiable {
+ let id: Int
+ let name: String
+ let categoryId: Int
+}
--- Sources/HHModels/Product.swift
+++ Sources/HHModels/Product.swift
@@ -5,9 +5,5 @@
 //  Created by Maciej on 29/12/2025.
 //
 
-struct Product: Identifiable {
- let id: Int
- let name: String
- let categoryId: Int
-}
+
 
