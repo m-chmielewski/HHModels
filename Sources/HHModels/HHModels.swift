551adc5765279ed0dac9d85c1a13a55c531a0d38
mchm <m@mchm.pl>
2025-12-29 16:51:47 +0100

    Bug fix

--- Sources/HHModels/HHModels.swift
+++ Sources/HHModels/HHModels.swift
@@ -5,4 +5,10 @@ public struct Product: Identifiable {
  public let id: Int
  public let name: String
  public let categoryId: Int
+ 
+ public init(id: Int, name: String, categoryId: Int) {
+  self.id = id
+  self.name = name
+  self.categoryId = categoryId
+ }
 }
